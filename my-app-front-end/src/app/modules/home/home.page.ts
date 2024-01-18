ebe42e2617da25780da421300587f6532fe841b7
DuclosAlex <duclos.alex85@outlook.fr>
2024-01-18 17:12:17 +0100

    finish footer, start work on register/login page

--- my-app-front-end/src/app/modules/home/home.page.ts
+++ my-app-front-end/src/app/modules/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { MainHomeComponent } from './components/main/main.component';
+import { Footer } from '../footer/footer.component';
 
 @Component({
     selector: 'home-page',
     standalone: true,
-    imports: [HeaderComponent, MainHomeComponent],
+    imports: [HeaderComponent, MainHomeComponent, Footer],
     templateUrl: './home.page.html',
     styleUrl: './home.page.css'
 })
