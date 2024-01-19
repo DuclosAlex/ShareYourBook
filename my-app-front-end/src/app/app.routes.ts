ed869130f8b2b50df2118767da57fe672dc615e8
DuclosAlex <duclos.alex85@outlook.fr>
2024-01-19 15:52:30 +0100

    finish login page basic, add to start work on back-end

--- my-app-front-end/src/app/app.routes.ts
+++ my-app-front-end/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './modules/home/home.page';
 import { Register } from './modules/register/register.page';
+import { Login } from './modules/login/login.page';
 
 export const routes: Routes = [
     {path: '', component: HomePage},
     {path: 'register', component : Register},
+    {path : 'login', component : Login}
 ];
---
+++ my-app-front-end/src/app/modules/login/login.page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'login',
+    standalone :true,
+    templateUrl : './login.page.html',
+    styleUrl : './login.page.css'
+})
+
+export class Login {}
