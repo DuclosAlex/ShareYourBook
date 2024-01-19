a6055234d41e2efabaabd4c0280a60ed6fbf51ab
DuclosAlex <duclos.alex85@outlook.fr>
2024-01-19 15:35:50 +0100

    finish basic of register page

--- my-app-front-end/src/app/app.routes.ts
+++ my-app-front-end/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './modules/home/home.page';
+import { Register } from './modules/register/register.page';
 
 export const routes: Routes = [
     {path: '', component: HomePage},
+    {path: 'register', component : Register},
 ];
